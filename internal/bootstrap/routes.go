ada85bce863f9e8734258ee69fecef3218f58a3c
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 10:30:33 +0700

    refactor: update route setup for improved organization and consistency

--- internal/bootstrap/routes.go
+++ internal/bootstrap/routes.go
@@ -4,38 +4,47 @@ import (
 	"be-ayaka/config"
 	"be-ayaka/internal/delivery/http"
 	"be-ayaka/internal/middleware"
-	"be-ayaka/pkg/utils"
+	"be-ayaka/pkg/generateid"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"gorm.io/gorm"
+	"github.com/gofiber/swagger"
 )
 
 func SetupRoutes(app *fiber.App, cfg *config.Config, db *gorm.DB) {
 	// middleware request
 	app.Use(requestid.New(requestid.Config{
 		Header:     fiber.HeaderXRequestID,
-		ContextKey: "request_id",
+		ContextKey: "requestid",
 		Generator: func() string {
-			return utils.GenerateID("REQUEST")
+			return generateid.GenerateID("REQUEST")
 		},
 	}))
 
-	// Health Check
+	// === Swagger UI
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	// === Health Check
 	healthHandler := http.NewHealthCheckHandler(cfg, db)
-	// health route
+	// --- health route
 	app.Get("/health", healthHandler.Check)
 	// ---
 
-	// version & auth
-	apiApp := app.Group("/api/v1")
-	auth := apiApp.Group("/", middleware.RequireAuth(cfg))
+	// === Handler ===
+	handler := BuildAllDependencies(db, cfg)
 
-	// User and Admin routes
-	userArea := auth.Group("/user", middleware.OnlyRole("user", "admin"))
+	// === api app group
+	apiApp := app.Group("/api")
 
-	// user
-	userHandler := BuildUserHandler(db)
+	// === version 1
+	apiV1 := apiApp.Group("/v1")
 
-	userArea.Get("/profile", userHandler.GetProfile)
+	// === auth group
+	authGroup := apiV1.Group("/", middleware.RequireAuth(cfg))
+
+	// === User and Admin routes
+	userGroup := authGroup.Group("/user", middleware.OnlyRole("user", "admin"))
+
+	userGroup.Get("/profile", handler.User.GetProfile)
 }
