4ca650158c87c76581c319b8364d7beb475b74b4
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 13:32:13 +0700

    refactor: rename Name field to Username in User struct for clarity

--- internal/core/entity/user.go
+++ internal/core/entity/user.go
@@ -2,8 +2,8 @@ package entity
 
 type User struct {
 	BaseEntity
-	Name string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	Username string `json:"username"`
+	Email    string `json:"email" gorm:"unique"`
 	Password string `json:"-"`
-	Role string `json:"role"`
+	Role     string `json:"role"`
 }
