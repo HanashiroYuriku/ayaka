4110e6f5441555bea7e132d739558184b1291a9a
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 10:19:42 +0700

    refactor: simplify User struct by removing redundant fields

--- internal/core/entity/user.go
+++ internal/core/entity/user.go
@@ -1,13 +1,9 @@
 package entity
 
-import "time"
-
 type User struct {
-	ID string `json:"id" gorm:"primaryKey"`
+	BaseEntity
 	Name string `json:"name"`
 	Email string `json:"email" gorm:"unique"`
 	Password string `json:"-"`
 	Role string `json:"role"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
 }
