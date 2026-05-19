e003dcfd865e4e8bcb63627b083242338eda8542
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 13:32:04 +0700

    feat: add UserRequest struct for user authentication

---
+++ internal/core/entity/auth.go
@@ -0,0 +1,8 @@
+package entity
+
+type UserRequest struct {
+	Username    string `json:"username" validate:"required,min=3,max=20,unique=users->username,whitespace,username" example:"hanashiroyuriku"`
+	Email       string `json:"email" validate:"required,max=100,email,unique=users->email,whitespace" example:"hanashiroyuriku@gmail.com"`
+	DisplayName string `json:"displayName" example:"Hanashiro Yuriku"`
+	Password    string `json:"password" validate:"required,complexpassword" example:"P4$$word"`
+}
